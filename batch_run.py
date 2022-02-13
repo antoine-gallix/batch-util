2bb99aa73c1ef9c84bc12d4d7d38740a645c09c7
Antoine Gallix <antoine@antoine-gallix.me>
2022-02-13 19:01:00 +0100

    more options

--- batch_run.py
+++ batch_run.py
@@ -1,33 +1,37 @@
 import pathlib
 import subprocess
-
+import functools
 import click
-
+import itertools
 
 @click.command()
 @click.argument(
-    "from_dir",
+    "origin",
     type=click.Path(
         exists=True,
         file_okay=False,
         path_type=pathlib.Path,
     ),
 )
-@click.argument(
-    "to_dir",
+@click.option(
+    "--dest",
     type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path),
+    help="Directory that will receive the processed files",
 )
-@click.argument(
-    "command_template",
+@click.option(
+    "--command",
     type=click.STRING,
+    help="Command to run on the input batch. Use two {} placeholders for the input and output files",
 )
-@click.option("--run",help='Run the commands. By default, the command do not run anything', is_flag=True)
-def run(from_dir, to_dir, command_template, run):
+@click.option(
+    "--run",
+    help="Run the commands. By default, the command do not run anything",
+    is_flag=True,
+)
+def run(origin, dest, command, run):
     """Apply commands in batch
 
-    FROM_DIR: Directory that contain the input files
-    TO_DIR: Directory that will receive the processed files
-    COMMAND_TEMPLATE: Command to run on the input batch. Use two {} placeholders for the input and output files
+    ORIGIN: Directory that contain the input files
 
     Use case:
 
@@ -42,9 +46,9 @@ def run(from_dir, to_dir, command_template, run):
     clean-audio --denoise 16 --normalize recordings/intro.ogg processed/intro.ogg
     clean-audio --denoise 16 --normalize recordings/interview.ogg processed/interview.ogg
     clean-audio --denoise 16 --normalize recordings/credits.ogg processed/credits.ogg
-    
+
     # run commands
-    > batch recordings processed 'clean-audio --denoise 16 --normalize {} {}' --run    
+    > batch recordings processed 'clean-audio --denoise 16 --normalize {} {}' --run
 
     clean-audio --denoise 16 --normalize recordings/intro.ogg processed/intro.ogg
     processing audio file...
@@ -54,19 +58,39 @@ def run(from_dir, to_dir, command_template, run):
     processing audio file...
     ```
     """
-    for file_in, file_out in file_pairs(from_dir, to_dir):
-        command = command_template.format(escape(file_in), escape(file_out))
-        print(command)
-        if run:
-            subprocess.call(command, shell=True)
+    if not dest and not command:
+        for file_in in origin.iterdir():
+            print(file_in)
+    elif not dest and command:
+        for file_in in origin.iterdir():
+            command = command.format(escape(file_in))
+            print(command)
+    elif dest and not command:
+        files_in,files_in_prime = itertools.tee(origin.iterdir())
+        files_out = map(functools.partial(substitute_dir, dest), files_in_prime)
+        for file_in, file_out in zip(files_in, files_out):
+            print(file_in, file_out)
+    elif dest and command:
+        files_in,files_in_prime = itertools.tee(origin.iterdir())
+        files_out = map(functools.partial(substitute_dir, dest), files_in_prime)
+        for file_in, file_out in zip(files_in, files_out):
+            command = command.format(escape(file_in), escape(file_out))
+            print(command)
+    else:
+        raise
+
 
 def escape(path):
     return '"' + str(path) + '"'
 
-def file_pairs(from_dir, to_dir):
-    for file_in in from_dir.iterdir():
-        file_out = to_dir / file_in.name
-        yield file_in, file_out
+
+def substitute_dir(dir_path, file_path):
+    return dir_path / file_path.name
+
+
+def file_pairs(origin, destination):
+    for file_in in origin.iterdir():
+        yield file_in, substitute_dir(file_in, destination)
 
 
 if __name__ == "__main__":
