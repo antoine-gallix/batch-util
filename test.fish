9fdd00b728bf2390d2ab5632cd6bb14401b7364c
Antoine Gallix <antoine@antoine-gallix.me>
2022-02-14 14:32:06 +0100

    reorder manual test script

--- test.fish
+++ test.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+# only one dir, no command.
+set command python batch_run.py $INPUTDIR
+echo $command
+eval $command
+
+# only one dir, with command. not yet working
+set command "python batch_run.py $INPUTDIR --command 'command --option {}'"
+echo $command
+eval $command
+
 # with a command
 set command python batch_run.py $INPUTDIR $OUTPUTDIR "command --option {0} {1}"
 echo $command
@@ -10,12 +20,3 @@ set command python batch_run.py $INPUTDIR $OUTPUTDIR
 echo $command
 eval $command
 
-# only one dir, no command.
-set command python batch_run.py $INPUTDIR
-echo $command
-eval $command
-
-# only one dir, with command. not yet working
-set command "python batch_run.py $INPUTDIR --command 'command --option {}'"
-echo $command
-eval $command
