3462211803e61958f2d96881e1be3ce61d264741
Antoine Gallix <antoine@antoine-gallix.me>
2022-02-14 17:44:29 +0100

    clean manual test

--- test.fish
+++ test.fish
@@ -5,18 +5,23 @@ set command python batch_run.py $INPUTDIR
 echo $command
 eval $command
 
+echo \n\n
+
 # only one dir, with command. not yet working
 set command "python batch_run.py $INPUTDIR --command 'command --option {}'"
 echo $command
 eval $command
 
-# with a command
-set command python batch_run.py $INPUTDIR $OUTPUTDIR "command --option {0} {1}"
+echo \n\n
+
+# output dir, without a command
+set command 'python batch_run.py $INPUTDIR --dest $OUTPUTDIR'
 echo $command
 eval $command
 
-# without a command. not yet working
-set command python batch_run.py $INPUTDIR $OUTPUTDIR
+echo \n\n
+
+# output dir, with a command
+set command 'python batch_run.py $INPUTDIR --dest $OUTPUTDIR --command "command --source {0} {1}"'
 echo $command
 eval $command
-
